ca9f2f808e17fe3ca75cd895c0f8e26c78631e91
gdownes <gdownes@gmail.com>
2017-05-09 08:38:40 +0100

    Testing sns publishing, chucking and acquiring checks

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const AWS = require("aws-sdk");
 const axios = require('axios');
 
-
 const chunk = (array, size) => {
     let results = [];
     while (array.length) {
@@ -14,9 +13,9 @@ const chunk = (array, size) => {
 
 module.exports.checkAcquire = (event, context, callback) => {
     const region = process.env.AWS_DEFAULT_REGION;
-
+    const topic = process.env.CHECKS_TOPIC;
     AWS.config.update({region: region});
-
+    const sns = new AWS.SNS();
     const http = axios.create({
         baseURL: process.env.API_URL,
         timeout: 1000
@@ -25,14 +24,23 @@ module.exports.checkAcquire = (event, context, callback) => {
     http.post('/candidate', {
         region: region
     }).then((response) => {
-        chunk(response, process.env.CHUNK_SIZE).map((checks) => {
-            new AWS.SNS().publish({
-                Message: checks,
-                TopicArn: process.env.CHECKS_TOPIC
+        if (response.data.length) {
+            Promise.all(chunk(response.data, process.env.CHUNK_SIZE).map((checks) => {
+                const checksJson = JSON.stringify(checks);
+                console.log(`Publishing ${checksJson} to ${topic}`);
+                sns.publish({
+                    Message: checksJson,
+                    TopicArn: topic
+                }).promise();
+            })).then(() => {
+                callback();
+            }).catch((reason) => {
+                console.log(reason)
             });
-        });
+        }
         callback();
     }).catch((error) => {
+        console.log(`error: ${error}`);
         callback(error);
     });
 };
